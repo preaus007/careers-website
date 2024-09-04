9289f01ff5826559841be269a0869025fe668a18
Touhedul Islam <touhedcs@gmail.com>
2024-09-04 05:41:51 +0000

    dynamically add jobs

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    "title": "Data Analyst",
+    "company": "Tech Company",
+    "location": "San Francisco, CA",
+    "salary": "$100,000 - $120,000"
+  },
+  {
+    "title": "Sodtware Engineer",
+    "company": "Tech Company",
+    "location": "San Francisco, CA",
+    "salary": "$100,000 - $120,000"
+  },
+  {
+    "title": "Marketing Head",
+    "company": "Tech Company",
+    "location": "San Francisco, CA",
+    "salary": "$100,000 - $120,000"
+  },
+  {
+    "title": "Cleaner",
+    "company": "Tech Company",
+    "location": "San Francisco, CA"
+  }
+]
 
 @app.route('/')
 def home():
-  return 'Hello, World!'
+  return render_template('index.html', jobs=JOBS)
 
 
 if __name__ == "__main__":
